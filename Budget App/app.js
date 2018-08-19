9ac2698158393fbaf1defc46cfb2861649310dd7
Garrett Pierce <gfpierce2@gmail.com>
2018-08-19 17:13:09 -0400

    Reading Input Data
    -Coded to read input data

---
+++ Budget App/app.js
@@ -0,0 +1,51 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
+
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector('.add__type').value, // Either inc or exp
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            };
+            
+        }
+    }
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    var ctrlAddItem = function() {
+        // TODO
+        // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
+
+        // 2. Add the item to the budget controller
+
+
+        // 3. Add the item to the UI
+
+
+        // 4. Calculate the budget
+
+
+        // 5. Display the budget on the UI
+
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+})(budgetController, UIController);
