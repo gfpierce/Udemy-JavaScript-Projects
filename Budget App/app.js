c1b88b8c028dbc56869e2a47c927c8fca964967d
Garrett Pierce <gfpierce2@gmail.com>
2018-08-22 21:35:29 -0400

    Add New Item to Budget Controller
    -app.js coded through lecture that shares name with commit title
    -Changed index.html to reflect the type names in app.js

--- Budget App/app.js
+++ Budget App/app.js
@@ -1,34 +1,109 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push into data structure
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+        testing: function() {
+            console.log(data);
+        }
+    };
+
 })();
 
 // UI CONTROLLER
 var UIController = (function() {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
     return {
-        getinput: function() {
+        getInput: function() {
             return {
-                type: document.querySelector('.add__type').value, // Either inc or exp
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value, // Either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             };
             
+        },
+
+        getDOMStrings: function() {
+            return DOMstrings;
         }
-    }
+    };
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    
+
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
     var ctrlAddItem = function() {
+        var input, newItem;
+        
         // TODO
         // 1. Get the field input data
-        var input = UICtrl.getinput();
-        console.log(input);
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
-
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
@@ -39,13 +114,14 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 5. Display the budget on the UI
 
 
-    }
+    };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log("Application has started.");
+            setupEventListeners();
         }
-    });
+    };
 })(budgetController, UIController);
+
+controller.init();
